566b38313eef554326c1646a776f061473a6607d
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-20 17:44:15 +0000

    Count question implementation. Test for counting group questions, with simplest implementation

--- day_6.rkt
+++ day_6.rkt
@@ -3,7 +3,12 @@
 (require rackunit)
 
 (define (count-questions questions)
+  (length questions))
+
+(define (count-groups-questions groups)
   3)
 
 ;Tests
 (check-equal? (count-questions '(a b c)) 3)
+(check-equal? (count-groups-questions '('(a b)
+                                        '(a c))) 3)
