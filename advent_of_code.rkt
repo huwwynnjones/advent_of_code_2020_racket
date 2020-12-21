6bc155a13e3f8bc0d0b701285e6c0d46a91ff59e
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-21 23:09:45 +0000

    Added test and code to count groups. It was wrong and so began a long search into Racket documentation to find out how to added a list to a list of lists... Answer found, test cases updated and new working implementation added. Difficult to remember to sort all that out in micro-commits.

--- advent_of_code.rkt
+++ advent_of_code.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
-(require "day_1.rkt")
-(require (rename-in "day_5.rkt" [load-input-file day-5-load-input-file]))
+(require "day_1.rkt"
+         (rename-in "day_5.rkt" [load-input-file day-5-load-input-file])
+         (rename-in "day_6.rkt" [load-input-file day-6-load-input-file]))
 
 (displayln "Day 1 part 1")
 (define input (load-input-file "day_1.txt"))
@@ -14,3 +15,8 @@
 (displayln (format "Answer is ~a" day-5-answer))
 (displayln "Day 5 part 2")
 (displayln (format "Answer is ~a" (missing-seat day-5-input)))
+
+(displayln "Day 6 part 1")
+(define day-6-input (day-6-load-input-file "day_6.txt"))
+(define day-6-answer (count-all-groups-questions day-6-input))
+(displayln (format "Answer is ~a" day-6-answer))
--- day_6.rkt
+++ day_6.rkt
@@ -2,29 +2,56 @@
 
 (require rackunit)
 
+(provide count-all-groups-questions
+         load-input-file)
+
 (define (count-questions questions)
   (length questions))
 
-(define (count-groups-questions groups)
+(define (convert-to-chars-no-duplicates questions)
+  (remove-duplicates
+   (flatten
+    (for/list ([q questions])
+      (string->list q)))))
+
+(define (count-group-questions group)
   (count-questions
-   (remove-duplicates
-    (flatten groups))))
+   (convert-to-chars-no-duplicates group)))
+
+(define (count-all-groups-questions groups)
+  (for/sum ([group groups])
+    (count-group-questions group)))
+
+(define (extend old new)
+  (foldr cons (list new) old))
+
+(define (split-into-groups items)
+  (for/fold ([groups null]
+             [group null]
+             #:result (extend groups group))
+            ([item items])
+    (cond
+      [(= (string-length item) 0)
+       (values (extend groups group) null)]
+      [else
+       (values groups (append group (list item)))])))
 
 (define (load-input-file file-name)
   (call-with-input-file file-name
     (λ (in)
-      (for/list ([line (in-lines in)]
-                 #:when (not-empty? line))
-        (string->list line)))))
-
-(define (not-empty? line)
-  (not (= (string-length line) 0)))
+      (split-into-groups (in-lines in)))))
 
 ;Tests
 (check-equal? (count-questions '("a" "b" "c")) 3)
-(check-equal? (count-groups-questions '(("a" "b")
-                                        ("a" "c"))) 3)
+(check-equal? (count-group-questions '("ab" "ac")) 3)
 (check-equal? (load-input-file
                "day_6_test.txt")
-              `(,(string->list "lqhksfnerg")
-                ,(string->list "negsc")))
+              '(("abc")
+                ("a" "b" "c")
+                ("ab" "ac")
+                ("a" "a" "a" "a")
+                ("b")))
+(define test-groups
+  (load-input-file "day_6_test.txt"))
+(check-equal? (count-all-groups-questions test-groups) 11)
+                  
