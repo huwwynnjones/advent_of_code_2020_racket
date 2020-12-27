da330303e3d897dbf473ac625677146176158381
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-27 20:16:07 +0000

    Test to split up the rule text, with implementation.
    Small functions added via REPL.

--- day_7.rkt
+++ day_7.rkt
@@ -3,13 +3,40 @@
 (require rackunit)
 
 (define (split-text text)
-  '("light red" (list (list 1 "bright white") (list 2 "muted yellow"))))
+  (if (third-bag-section? text)
+      (list (first-bag text)
+            (list (second-bag text)
+                  (third-bag text)))
+      (list (first-bag text)
+            (list (second-bag text)))))
 
+(define (first-bag text)
+  (let* ([words (string-split text)]
+         [hue (first words)]
+         [colour (second words)])
+    (string-join (list hue colour))))
+
+(define (second-bag text)
+  (let* ([words (string-split text)]
+         [number (fifth words)]
+         [hue (sixth words)]
+         [colour (seventh words)])
+    (list (string->number number) (string-join (list hue colour)))))
+
+(define (third-bag text)
+  (let* ([third-bag-section (string-split (second (string-split text ",")))]
+         [number (first third-bag-section)]
+         [hue (second third-bag-section)]
+         [colour (third third-bag-section)])
+    (list (string->number number) (string-join (list hue colour)))))
+
+(define (third-bag-section? text)
+  (string-contains? text ","))
+    
 
 
 ;Tests
-(check-equal? (split-text
-               "light red bags contain 1 bright white bag, 2 muted yellow bags."
-               )
-              '("light red" (list (list 1 "bright white")
+(define test-rule "light red bags contain 1 bright white bag, 2 muted yellow bags.")
+(check-equal? (split-text test-rule)
+              (list "light red" (list (list 1 "bright white")
                                   (list 2 "muted yellow"))))
