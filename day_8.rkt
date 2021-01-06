0674d6a3af848421542d9ffeef86ad09ed26f9b4
Huw Wynn-Jones <huw@huwwynnjones.com>
2021-01-06 23:48:59 +0000

    Refactor and test for loading input program.

--- day_8.rkt
+++ day_8.rkt
@@ -4,10 +4,21 @@
 
 (define (string->op-pair str)
   (let ([str-list (string-split str)])
-    (cons (car str-list) (string->number (cadr str-list)))))
+    (cons (string->op-symbol (car str-list))
+          (string->number (cadr str-list)))))
+
+(define (string->op-symbol str)
+  (match str
+    ["nop" 'nop]))
 
 
 ; Tests
 (test-case
     "Read string into op pair"
-  (check-equal? (string->op-pair "nop +0") '("nop" . 0)))
+  (check-equal? (string->op-pair "nop +0") '(nop . 0)))
+(test-case
+    "Load program into vector"
+  (check-equal? (load-input-file "day_8_test.rkt")
+                (`(
+                   ,'(nop 0)
+                   ,'(acc 1)))))
