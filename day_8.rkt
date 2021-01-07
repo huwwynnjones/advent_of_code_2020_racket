f6b9d8b554aa5a8150fc82a077186b6dbcf57cd0
Huw Wynn-Jones <huw@huwwynnjones.com>
2021-01-07 23:16:54 +0000

    Test for running the program with simplest implementation.

--- day_8.rkt
+++ day_8.rkt
@@ -9,8 +9,18 @@
 
 (define (string->op-symbol str)
   (match str
-    ["nop" 'nop]))
+    ["nop" 'nop]
+    ["acc" 'acc]
+    ["jmp" 'jmp]))
 
+(define (load-input-file filename)
+  (call-with-input-file filename
+    (λ (in)
+      (for/list ([line (in-lines in)])
+        (string->op-pair line)))))
+
+(define (run-program program)
+  5)
 
 ; Tests
 (test-case
@@ -19,6 +29,17 @@
 (test-case
     "Load program into vector"
   (check-equal? (load-input-file "day_8_test.rkt")
-                (`(
-                   ,'(nop 0)
-                   ,'(acc 1)))))
+                `(
+                  ,'(nop . 0)
+                  ,'(acc . 1)
+                  ,'(jmp . 4)
+                  ,'(acc . 3)
+                  ,'(jmp . -3)
+                  ,'(acc . -99)
+                  ,'(acc . 1)
+                  ,'(jmp . -4)
+                  ,'(acc . 6))))
+(test-case
+    "Accumulator is 5 before any instruction is run a second time"
+  (let ([program (load-input-file "day_8_test.rkt")])
+    (check-equal? (run-program program) 5)))
